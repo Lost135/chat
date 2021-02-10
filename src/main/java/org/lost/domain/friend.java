b5f65220fe3344553008c1d296003294fe72dee7
lost <1420335820@qq.com>
2021-02-10 20:58:14 +0800

    1.0.1R

---
+++ src/main/java/org/lost/domain/friend.java
@@ -0,0 +1,49 @@
+package org.lost.domain;
+
+public class friend {
+    private String id;
+    private String userId;
+    private String friendId;
+    private String comments;
+    private String createTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(String friendId) {
+        this.friendId = friendId;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
